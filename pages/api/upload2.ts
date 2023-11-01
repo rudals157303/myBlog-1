debf8e916f234dbcb88db0d0bb8dda463fdf2e8a
kmjang <rudals782@nate.com>
2023-11-01 23:06:12 +0900

    ddd

--- pages/api/upload2.ts
+++ pages/api/upload2.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import formidable from "formidable";
-import path from "path";
-import fs from "fs/promises";
+// import formidable from "formidable";
+// import path from "path";
+// import fs from "fs/promises";
 
 export const config = {
   api: {
@@ -11,40 +11,40 @@ export const config = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    console.log("ddddd");
+    // console.log("ddddd");
 
-    const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
-    try {
-      await fs.readdir(imgStoragePath);
-    } catch {
-      await fs.mkdir(imgStoragePath);
-    }
+    // const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
+    // try {
+    //   await fs.readdir(imgStoragePath);
+    // } catch {
+    //   await fs.mkdir(imgStoragePath);
+    // }
 
-    const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
-      const options: formidable.Options = {};
+    // const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
+    //   const options: formidable.Options = {};
 
-      if (saveLocally) {
-        //true일때 option객체에 path와 filename을 저장
-        options.uploadDir = imgStoragePath;
-        options.filename = (name, ext, path) => {
-          return Date.now().toString() + "_" + path.originalFilename;
-        };
-      }
-      return new Promise<{
-        fields: formidable.Fields;
-        files: formidable.Files;
-      }>((resolve, rejects) => {
-        const form = formidable(options);
-        form.parse(req, (err, fields, files) => {
-          if (err) {
-            rejects(err);
-          }
-          resolve({ fields, files });
-        });
-      });
-    };
-    const data = await readFile(req, true);
-    console.log(data.files.image, "data");
+    //   if (saveLocally) {
+    //     //true일때 option객체에 path와 filename을 저장
+    //     options.uploadDir = imgStoragePath;
+    //     options.filename = (name, ext, path) => {
+    //       return Date.now().toString() + "_" + path.originalFilename;
+    //     };
+    //   }
+    //   return new Promise<{
+    //     fields: formidable.Fields;
+    //     files: formidable.Files;
+    //   }>((resolve, rejects) => {
+    //     const form = formidable(options);
+    //     form.parse(req, (err, fields, files) => {
+    //       if (err) {
+    //         rejects(err);
+    //       }
+    //       resolve({ fields, files });
+    //     });
+    //   });
+    // };
+    // const data = await readFile(req, true);
+    // console.log(data.files.image, "data");
 
     res.status(200).json({ message: "이미지가 업로드되었습니다." });
   }
