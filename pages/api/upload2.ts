fcf5853125cc7c86f250849061a6df0172b94d6e
kmjang <rudals782@nate.com>
2023-11-01 23:26:22 +0900

    ..

--- pages/api/upload2.ts
+++ pages/api/upload2.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-// import formidable from "formidable";
+import formidable from "formidable";
 import path from "path";
 // import fs from "fs/promises";
 
@@ -17,33 +17,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   //   await fs.mkdir(imgStoragePath);
   // }
 
-  // const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
-  //   const options: formidable.Options = {};
+  const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
+    const options: formidable.Options = {};
 
-  //   if (saveLocally) {
-  //     //true일때 option객체에 path와 filename을 저장
-  //     options.uploadDir = imgStoragePath;
-  //     options.filename = (name, ext, path) => {
-  //       return Date.now().toString() + "_" + path.originalFilename;
-  //     };
-  //   }
-  //   return new Promise<{
-  //     fields: formidable.Fields;
-  //     files: formidable.Files;
-  //   }>((resolve, rejects) => {
-  //     const form = formidable(options);
-  //     form.parse(req, (err, fields, files) => {
-  //       if (err) {
-  //         rejects(err);
-  //       }
-  //       resolve({ fields, files });
-  //     });
-  //   });
-  // };
-  // const data = await readFile(req, true);
-  // console.log(data.files.image, "data");
+    if (saveLocally) {
+      //true일때 option객체에 path와 filename을 저장
+      options.uploadDir = imgStoragePath;
+      options.filename = (name, ext, path) => {
+        return Date.now().toString() + "_" + path.originalFilename;
+      };
+    }
+    return new Promise<{
+      fields: formidable.Fields;
+      files: formidable.Files;
+    }>((resolve, rejects) => {
+      const form = formidable(options);
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          rejects(err);
+        }
+        resolve({ fields, files });
+      });
+    });
+  };
+  const data = await readFile(req, true);
 
-  res
-    .status(200)
-    .json({ message: "이미지가 업로드되었습니다.4", imgStoragePath });
+  res.status(200).json({ message: "이미지가 업로드되었습니다.4", data });
 };
