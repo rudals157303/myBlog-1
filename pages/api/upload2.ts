2d6bb0473c52a3842d8bc5e377c91bd668ae6d3b
kmjang <rudals782@nate.com>
2023-11-01 23:16:39 +0900

    22

--- pages/api/upload2.ts
+++ pages/api/upload2.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // import formidable from "formidable";
-// import path from "path";
-// import fs from "fs/promises";
+import path from "path";
+import fs from "fs/promises";
 
 export const config = {
   api: {
@@ -11,12 +11,12 @@ export const config = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    // const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
-    // try {
-    //   await fs.readdir(imgStoragePath);
-    // } catch {
-    //   await fs.mkdir(imgStoragePath);
-    // }
+    const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
+    try {
+      await fs.readdir(imgStoragePath);
+    } catch {
+      await fs.mkdir(imgStoragePath);
+    }
 
     // const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
     //   const options: formidable.Options = {};
@@ -44,6 +44,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     // const data = await readFile(req, true);
     // console.log(data.files.image, "data");
 
-    res.status(200).json({ message: "이미지가 업로드되었습니다.2" });
+    res.status(200).json({ message: "이미지가 업로드되었습니다.3" });
   }
 };
