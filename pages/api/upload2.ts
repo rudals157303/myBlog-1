91bd70a74b36ed55d9148a8b32b4e74af179f096
kmjang <rudals782@nate.com>
2023-11-02 22:44:19 +0900

    ..

--- pages/api/upload2.ts
+++ pages/api/upload2.ts
@@ -9,40 +9,41 @@ export const config = {
   },
 };
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
-    try {
-      await fs.readdir(imgStoragePath);
-    } catch {
-      await fs.mkdir(imgStoragePath);
-    }
-    const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
-      const options: formidable.Options = {};
-
-      if (saveLocally) {
-        //true일때 option객체에 path와 filename을 저장
-        options.uploadDir = imgStoragePath;
-        options.filename = (name, ext, path) => {
-          return Date.now().toString() + "_" + path.originalFilename;
-        };
-      }
-      return new Promise<{
-        fields: formidable.Fields;
-        files: formidable.Files;
-      }>((resolve, rejects) => {
-        const form = formidable(options);
-        form.parse(req, (err, fields, files) => {
-          if (err) {
-            rejects(err);
-          }
-          resolve({ fields, files });
-        });
-      });
-    };
-    const data = await readFile(req, true);
-
-    res.status(200).json({ message: "이미지가 업로드되었습니다.", data });
-  } else {
-    return res.status(405).end();
+  // if (req.method === "POST") {
+  const imgStoragePath = path.join(process.cwd() + "/public" + "/images");
+  try {
+    await fs.readdir(imgStoragePath);
+  } catch {
+    await fs.mkdir(imgStoragePath);
   }
+  const readFile = (req: NextApiRequest, saveLocally: boolean = false) => {
+    const options: formidable.Options = {};
+
+    if (saveLocally) {
+      //true일때 option객체에 path와 filename을 저장
+      options.uploadDir = imgStoragePath;
+      options.filename = (name, ext, path) => {
+        return Date.now().toString() + "_" + path.originalFilename;
+      };
+    }
+    return new Promise<{
+      fields: formidable.Fields;
+      files: formidable.Files;
+    }>((resolve, rejects) => {
+      const form = formidable(options);
+      form.parse(req, (err, fields, files) => {
+        if (err) {
+          rejects(err);
+        }
+        resolve({ fields, files });
+      });
+    });
+  };
+  const data = await readFile(req, true);
+
+  res.status(200).json({ message: "이미지가 업로드되었습니다.", data });
+  // }
+  // else {
+  //   return res.status(405).end();
+  // }
 };
